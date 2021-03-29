d712e7bb3cad97a0dc77f1b15874b1a81700a8a4
Aleyucra74 <alexandre.yucra@outlook.com>
2021-03-28 23:36:12 -0300

    fixing bugs

--- index.js
+++ index.js
@@ -1,45 +1,42 @@
-var  container  =  document . getElementById ( "app" ) ;
+var  container  =  document.getElementById("app");
 
-função  Contador ( props ) {
+function  Contador( props ) {
 
-    const [ numero ,  setNumero ]  =  React . useState ( 0 ) ;
+    const [ numero, setNumero ]  =  React.useState(0);
 
-    function  somar ( ) {
-        setNumero ( numero  +  1 ) ;
-        console . log ( "add 1 ao nmr ="  +  numero ) ;
+    function  somar() {
+        setNumero( numero  +  1 ) ;
+        console.log( "add 1 ao nmr = "  +  numero );
     }
 
     
-    function  subtrair ( ) {
-        setNumero ( numero  -  1 ) ;
-        console . log ( "sub 1 ao nmr ="  +  numero ) ;
+    function  subtrair() {
+        setNumero( numero  -  1 );
+        console.log( "sub 1 ao nmr ="  +  numero );
     }
 
 
     return (
         <React.Fragment>
-            < h1 > { props.titulo } < / h1 >
-            < h2 > { numero } < / h2 >
-            < button  onClick = { somar } > + < / button >
-            < button  onClick = { subtrair } > - < / button >
+            <h1>{ props.titulo }</h1>
+            <h2>{ numero }</h2>
+            <button  onClick={ somar }> + </ button>
+            <button  onClick={ subtrair }> - </button>
         </React.Fragment>
         
     ) ;
 }
 
-function  App ( ) {
+function  App() {
     return  (
-        < React . Fragment >
-            < h1 > Vote< / h1 >
-            < Contador  titulo = "rock" / >
-            < Contador  titulo = "funk" / >
-            < Contador  titulo = "pop" / >
-        < / React . Fragment >        
+        <React.Fragment>
+            <h1> Vote</h1>
+            <Contador titulo = "rock"/>
+            <Contador titulo = "funk"/>
+            <Contador titulo = "pop"/>
+        </React.Fragment>        
     ) ;
 
 }
 
-ReactDOM . render ( React . createElement ( App , null , null ) ,  container ) ;
-
-
-//ReactDOM.render (App (), container);
+ReactDOM.render( React.createElement( App, null, null ), container );
