88bd038a0c89f036ad02461865051855d4715290
Aleyucra74 <alexandre.yucra@outlook.com>
2021-05-05 21:44:24 -0300

    simples routes with axios

---
+++ bootcamp-react-101--3CCO-2021-1/music-box/src/pages/MinhasMusicas.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import CardMusica from '../components/CardMusica';
+import NavBar from '../components/NavBar';
+
+import api from '../api';
+
+function MinhasMusicas() {
+
+    const [musicas, setMusicas] = useState([]);
+
+    useEffect(() => {
+        
+        async function getAllMusicas() {
+            const resposta = await api.get("/");
+
+            console.log(resposta.data);
+
+            setMusicas(resposta.data);
+        }
+                
+        getAllMusicas();
+
+    },[]);
+
+    return (
+        <>
+            <NavBar />
+            <div className="container">
+                <div className="filter">
+                    {/* adicionar button com filho, lado e destino aqui! */}
+                        <button className="btn-green right">Adicionar</button>
+                    {/* adicionar button com filho, lado e destino aqui! */}
+                </div>
+            </div>
+
+            <div className="container">
+                <div className="music-boxes">
+
+                {
+                    musicas.map((musica) => (
+                        <CardMusica nome={musica.nome} genero={musica.genero} ano={musica.ano}/>
+                    ))
+                }
+
+                <CardMusica nome="Assalto ao Trap" genero="Trap" ano="2020"/>
+
+                <CardMusica nome="Hatsune Miku" genero="jpop" ano="2020" />
+
+                <CardMusica nome="Melhor eu ir" genero="Pagode" ano="2015" />
+
+                <CardMusica nome="Na sua estante" genero="Emoxinho :3" ano="2000"/>
+
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default MinhasMusicas;
